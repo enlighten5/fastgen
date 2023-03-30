59a8aa1f09671c04d083c4a255b1f4e036d7e488
enlighten5 <zqi020@ucr.edu>
2023-03-30 00:04:33 -0700

    modify driver to fix incompatibility

--- usenix/fuzzer.sh
+++ usenix/fuzzer.sh
@@ -1,4 +1,5 @@
 num_seeds=$1
-pro=$2
-opt=$3
-RUST_LOG=info ./fastgen  -i /out/real_seeds/${pro}_reduced/ -s ${num_seeds} -o corpus_${pro} -t ./${pro}.symsan -- ./${pro}.symsan ${opt} @@
+target=$2
+pro=$3
+opt=$4
+RUST_LOG=info /fastgen/target/release/fastgen -i /out/real_seeds/${pro}_reduced/ -s ${num_seeds} -o corpus_${pro} -t /usr/bin/${target}-- /usr/bin/${target} ./${pro}.native ${opt} @@
